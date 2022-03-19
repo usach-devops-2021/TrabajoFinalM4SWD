1e5d70f16561ed0b5f3e90f018e198f17e7de6b5
Felipe Contreras <confedev@gmail.com>
2022-03-19 13:46:55 -0300

    Upload README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+### Run Jar
+* Local: mvn spring-boot:run
+
+### Compile Code
+* mvn clean compile -e
+
+### Test Code
+* mvn clean test -e
+
+### Jar Code
+* mvn clean package -e
+
+### Testing Application
+* Abrir navegador: http://localhost:8080/rest/msdxc/dxc?sueldo=4000&ahorro=5000
--- src/main/java/com/devops/dxc/devops/rest/RestData.java
+++ src/main/java/com/devops/dxc/devops/rest/RestData.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/rest/msdxc")
 public class RestData {
-	
+
 	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
 	@GetMapping(path = "/dxc", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Dxc getData(@RequestParam(name = "sueldo") String sueldo, @RequestParam(name = "ahorro") String ahorro){
-		
+
 		LOGGER.log(Level.INFO, "< Trabajo DevOps - DXC > <Consultado Diez por ciento>");
-		
+
         Dxc response = new Dxc(Integer.parseInt(ahorro), Integer.parseInt(sueldo));
 		return response;
 	}
