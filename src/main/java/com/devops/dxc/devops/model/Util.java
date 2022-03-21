2520f14491ba86341eb14d0175a0f373840cfaf8
Felipe Contreras <confedev@gmail.com>
2022-03-20 21:15:37 -0300

    Se agrega impuesto

--- src/main/java/com/devops/dxc/devops/model/Util.java
+++ src/main/java/com/devops/dxc/devops/model/Util.java
@@ -36,9 +36,31 @@ public class Util {
      * @return
      */
     public static int getImpuesto(int ahorro, int sueldo){
-        // Agregar cálculo.
-        int impuesto = (int)(ahorro * 0.19);
-        return impuesto;
+        double factor = 0;
+        int sueldoAnual = sueldo * 12;
+        int dxc = getDxc(ahorro, sueldo);
+        // System.out.println("El sueldo anual es: " + sueldoAnual);
+        if(sueldo >= 1500000){
+            if(sueldoAnual <= 8266698) {
+                factor = 0;
+            } else if (sueldoAnual <= 18370440) {
+                factor = 0.04;
+            } else if (sueldoAnual <= 30617400) {
+                factor = 0.08;
+            } else if (sueldoAnual <= 42864360) {
+                factor = 0.135;
+            } else if (sueldoAnual <= 55111320) {
+                factor = 0.23;
+            } else if (sueldoAnual <= 73481760) {
+                factor = 0.304;
+            } else if (sueldoAnual <= 189827880) {
+                factor = 0.35;
+            } else {
+                factor = 0.4;
+            }
+        }
+        // System.out.println("El factor es:" + factor);
+        return (int)(dxc * factor);
     }
 
     /**
@@ -51,7 +73,7 @@ public class Util {
      */
     public static int getSaldo(int ahorro, int sueldo){
         int dxc = getDxc(ahorro,sueldo);
-        int impuesto = getDxc(ahorro,sueldo);
+        int impuesto = getImpuesto(ahorro,sueldo);
         // Agregar cálculo.
         return ahorro - dxc - impuesto;
     }
@@ -62,14 +84,14 @@ public class Util {
      * @return
      */
     public static int getUf(){
-        System.out.println("Obteniendo UF");
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> call= restTemplate.getForEntity("https://mindicador.cl/api/uf" ,String.class);
-        Gson gson = new Gson();
+        // System.out.println("Obteniendo UF");
+        //RestTemplate restTemplate = new RestTemplate();
+        //ResponseEntity<String> call= restTemplate.getForEntity("https://mindicador.cl/api/uf" ,String.class);
+        //Gson gson = new Gson();
         //MiIndicador miIndicador = gson.fromJson(call.getBody().toLowerCase(), MiIndicador.class);
         //int valorUf = (miIndicador.getSerie().get(0).getValor()).intValue();
         int valorUf = 27000;
-        System.out.println("El valor de la UF es: " + valorUf);
+        // System.out.println("El valor de la UF es: " + valorUf);
         return valorUf;
     }
     
